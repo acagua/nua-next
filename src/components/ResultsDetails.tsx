bac543c753a552399d8cb162af67ea0228415de3
Andres Cagua <af.cagua1011@gmail.com>
2023-05-26 09:34:10 -0500

    add icon to high score and add dynamic colors

--- src/components/ResultsDetails.tsx
+++ src/components/ResultsDetails.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import PremiumIcon from "/public/Premium.svg";
 import LogoIcon from "/public/logo2.svg";
 import { CategoryResult } from "./CategoryResult";
 import { Results } from "../types/types";
@@ -19,14 +20,13 @@ const LOW_TOTAL_SCORE = 84;
 const MEDIUM_TOTAL_SCORE = 161;
 const MAX_TOTAL_SCORE = 200;
 
+enum Colors {
+  LOW = "#92d5ce",
+  MEDIUM = "#9f7eee",
+  HIGH = "#5325a0",
+}
 export const ResultsDetails = ({ data }: { data: Results }) => {
   const { name, attributes } = data;
-
-  const colors = {
-    low: "#92d5ce",
-    medium: "#9f7eee",
-    high: "#5325a0",
-  };
 
   const { relational, emotional, mental, physical, financial, burnout } =
     attributes;
@@ -36,16 +36,32 @@ export const ResultsDetails = ({ data }: { data: Results }) => {
     mental.score +
     physical.score +
     financial.score +
-    burnout.score;
+    burnout.score +
+    52;
 
+  let color = Colors.LOW;
   let totalMessage = totalScoreMessage.low;
   if (totalScore > LOW_TOTAL_SCORE) {
+    color = Colors.MEDIUM;
     totalMessage = totalScoreMessage.medium;
   }
   if (totalScore > MEDIUM_TOTAL_SCORE) {
+    color = Colors.HIGH;
     totalMessage = totalScoreMessage.high;
   }
 
+  const PremiumScore = () => {
+    if (totalScore > MEDIUM_TOTAL_SCORE) {
+      return (
+        <Image
+          className="relative col-span-full row-span-full w-6 bottom-12"
+          src={PremiumIcon}
+          alt="icon"
+        />
+      );
+    }
+    return <></>;
+  };
   return (
     <>
       <nav className="top-0 sticky  w-full">
@@ -53,26 +69,29 @@ export const ResultsDetails = ({ data }: { data: Results }) => {
           <Image src={LogoIcon} alt="icon" />
         </div>
       </nav>
-      <div className="pt-3 flex flex-col w-[45vw] min-w-[400px] flex-grow">
-        <h1 className="text-2xl font-title">
+      <div className="pt-3 flex flex-col w-[45vw] min-w-[400px] flex-grow justify-center items-center">
+        <h1 className="text-2xl font-title p-4">
           {name} aquí están tus resultados!
         </h1>
-        <div className="w-[200px]">
-          <CircularProgressbar
-            value={totalScore}
-            text={`${totalScore}`}
-            styles={buildStyles({
-              rotation: 0,
-              strokeLinecap: "round",
-              textSize: "25px",
-              // Colors
-              pathColor: colors.high,
-              textColor: colors.high,
-              trailColor: "#d6d6d6",
-              backgroundColor: "#3e98c7",
-            })}
-            maxValue={MAX_TOTAL_SCORE}
-          />
+        <div className="grid place-items-center w-[200px]">
+          <PremiumScore />
+          <div className="col-span-full row-span-full">
+            <CircularProgressbar
+              value={totalScore}
+              text={`${totalScore}`}
+              styles={buildStyles({
+                rotation: 0,
+                strokeLinecap: "round",
+                textSize: "25px",
+                // Colors
+                pathColor: color,
+                textColor: color,
+                trailColor: "#d6d6d6",
+                backgroundColor: "#3e98c7",
+              })}
+              maxValue={MAX_TOTAL_SCORE}
+            />
+          </div>
         </div>
         <p className="text-lg font-title font-bold"> Calificación general </p>
         <p className="font-text"> {totalMessage}</p>
